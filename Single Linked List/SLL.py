2598e69f93be15fa6e9331e9050c8323de06f1ec
Abhishek0104 <abhishek.raut.0104@gmail.com>
2017-10-18 17:14:10 +0530

    Functions Added

--- Single Linked List/SLL.py
+++ Single Linked List/SLL.py
@@ -16,6 +16,36 @@ class LinkedList:
         while temp:
             print(temp.data)
             temp = temp.next
+        print("....END....")
+
+    def insertatbeg(self, x):
+        new_node = Node(x)
+
+        new_node.next = self.head
+
+        self.head = new_node
+
+    def insertafter(self, prev_node, x):
+        tag, node = LinkedList.findnode(self, prev_node)
+        if tag is True:
+            new_node = Node(x)
+
+            new_node.next = node.next
+
+            node.next = new_node
+        else:
+            print("Previous element not found.....")
+
+    def findnode(self, x):
+        if self.head is None:
+            return None
+        else:
+            temp = self.head
+            while temp is not None:
+                if temp.data is x:
+                    return True, temp
+                temp = temp.next
+            return None
 
 
 if __name__ == '__main__':
@@ -30,3 +60,10 @@ if __name__ == '__main__':
     second.next = third
 
     llist.printlist()
+
+    llist.insertatbeg(4)
+    llist.printlist()
+
+    llist.insertafter(4, 2)
+    llist.printlist()
+
